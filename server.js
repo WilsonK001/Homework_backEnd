b04e638b02351f7b98554f5d2e940c0c726b9385
Wilson Kameni <wilsonk@wilsons-mbp.myfiosgateway.com>
2021-08-05 17:48:03 -0400

    backend

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const recipesController = require('./controllers/recipes.js')
+
+const cors = require('cors')
+
+
+
+
+
+
+
+
+
+
+//////MIDDLEWARE//////
+app.use(express.json())
+app.use('/recipes', recipesController)
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+//////ROUTES/////
+
+
+
+
+
+
+
+
+
+
+
+
+
+mongoose.connect(
+    'mongodb://localhost:27017/merncrud', 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useFindAndModify: false 
+    })
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongod...')
+})
+
+
+app.listen(3000, ()=>{
+    console.log('cooking...')
+})
